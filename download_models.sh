15d7452c3e9012c469b11a5d48597e3f89225f61
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-04 01:18:44 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Create a directory for checkpoints
-mkdir -p ./checkpoints
+# Create directory for pretrained models
+mkdir -p ./Pretrained-models
 
-# Install rclone if not installed
-sudo apt-get install -y rclone
+# Download entire folder (files retain original names)
+gdown "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4?usp=drive_link"
+gdown "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU?usp=drive_link"
+gdown "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0?usp=drive_link"
+gdown "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl?usp=drive_link"
+gdown "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS?usp=drive_link"
+gdown "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc?usp=drive_link"
+gdown "https://drive.google.com/uc?export=download&id=1uUU6iZ8CdgsCk3JAG6V7BhJXnfWpaQ7a"  # Individual file download
 
-# Configure rclone manually (or do this step earlier)
-# rclone config
+# Unzip downloaded folders into the pretrained models directory
+for f in ./models/*.zip; do
+    unzip -q $f -d ./Pretrained-models
+done
 
-# Copy files from Google Drive folder
-rclone copy gdrive:1sHc2xEEGwnb0h2rkUhG9sPmOxvRvPVpJ ./checkpoints/ --progress
-
-# List downloaded files
-ls -lh ./checkpoints/
+echo "All models downloaded and unzipped to ./Pretrained-models"
