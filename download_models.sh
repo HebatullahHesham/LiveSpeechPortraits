7dc7d0bd21bbad4074aa705505a6c0532d5fd848
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-04 02:10:55 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Create directory for pretrained models
+# Create a single directory for all pretrained models
 mkdir -p ./Pretrained-models
 
-# Download entire folder (files retain original names)
-gdown "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4?usp=drive_link"
-gdown "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU?usp=drive_link"
-gdown "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0?usp=drive_link"
-gdown "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl?usp=drive_link"
-gdown "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS?usp=drive_link"
-gdown "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc?usp=drive_link"
-gdown "https://drive.google.com/uc?export=download&id=1uUU6iZ8CdgsCk3JAG6V7BhJXnfWpaQ7a"  # Individual file download
+# Download individual folders into the Pretrained-models directory
+gdown --fuzzy --folder "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4" -O ./Pretrained-models
+gdown --fuzzy --folder "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU" -O ./Pretrained-models
+gdown --fuzzy --folder "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0" -O ./Pretrained-models
+gdown --fuzzy --folder "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl" -O ./Pretrained-models
+gdown --fuzzy --folder "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS" -O ./Pretrained-models
+gdown --fuzzy --folder "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc" -O ./Pretrained-models
 
-# Unzip downloaded folders into the pretrained models directory
-for f in ./models/*.zip; do
-    unzip -q $f -d ./Pretrained-models
-done
-
-echo "All models downloaded and unzipped to ./Pretrained-models"
+# Notify the user upon successful completion
+echo "All models downloaded and saved in ./Pretrained-models"
