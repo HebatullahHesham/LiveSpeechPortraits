f6223189d89c6ae75d318780da1e70bfd9f79598
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-05 20:27:25 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Create a single directory for all pretrained models
+# Create a single directory for the folder
 mkdir -p ./Pretrained-models
 
-# Download individual folders into the Pretrained-models directory
-gdown --fuzzy --folder "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4" -O ./Pretrained-models
-gdown --fuzzy --folder "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU" -O ./Pretrained-models
-gdown --fuzzy --folder "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0" -O ./Pretrained-models
-gdown --fuzzy --folder "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl" -O ./Pretrained-models
-gdown --fuzzy --folder "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS" -O ./Pretrained-models
-gdown --fuzzy --folder "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc" -O ./Pretrained-models
+# Google Drive folder link
+drive_folder_link="https://drive.google.com/drive/folders/1sHc2xEEGwnb0h2rkUhG9sPmOxvRvPVpJ"
 
-# Notify the user upon successful completion
-echo "All models downloaded and saved in ./Pretrained-models"
+# Download the folder and its contents
+gdown --fuzzy --folder "$drive_folder_link" -O ./Pretrained-models
