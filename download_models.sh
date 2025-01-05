1847a5585ad2d06323e24ba3caec2a2910aab60b
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-05 21:12:05 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -11,9 +11,9 @@ drive_folder_links=(
     "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0?usp"
     "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU?usp"
     "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc?usp"
-    "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU?usp"
+    "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU?"
     "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4?usp"
-    "https://drive.google.com/file/d/1uUU6iZ8CdgsCk3JAG6V7BhJXnfWpaQ7a"
+    "https://drive.google.com/file/d/1uUU6iZ8CdgsCk3JAG6V7BhJXnfWpaQ7a/view?"
 )
 
 # Loop through each folder link and download its contents
