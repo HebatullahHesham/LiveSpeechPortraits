ba502f07ac7ecfdd5d0b45ac054b02ad6b101261
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-06 20:32:52 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,8 +1,15 @@
-mkdir ./checkpoints  
+#!/bin/bash
 
-wget -nc "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS"
-wget -nc "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc"
-wget -nc "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl"
-wget -nc "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0"
-wget -nc "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU"
-wget -nc "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4"
+# Create the checkpoints directory
+mkdir -p ./checkpoints
+
+# Change to the checkpoints directory
+cd ./checkpoints
+
+# Download the folders using gdown
+gdown --folder "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS"
+gdown --folder "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc"
+gdown --folder "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl"
+gdown --folder "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0"
+gdown --folder "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU"
+gdown --folder "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4"
