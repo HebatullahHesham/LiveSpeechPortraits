d8275b53ccebdf960afbe1b7086cddf740535db4
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-06 19:23:41 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -5,14 +5,13 @@ mkdir -p ./Pretrained-models
 
 # List of Google Drive folder and file links
 drive_links=(
-    "https://drive.google.com/drive/folders/1sHc2xEEGwnb0h2rkUhG9sPmOxvRvPVpJ"
-    "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS"
-    "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl"
-    "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0"
-    "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU"
-    "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc"
-    "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4"
-    "https://drive.google.com/file/d/1uUU6iZ8CdgsCk3JAG6V7BhJXnfWpaQ7a"
+    "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS?usp=drive_link"
+    "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc?usp=drive_link"
+    "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl?usp=drive_link"
+    "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0?usp=drive_link"
+    "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU?usp=drive_link"
+    "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4?usp=drive_link"
+    "https://drive.google.com/file/d/1uUU6iZ8CdgsCk3JAG6V7BhJXnfWpaQ7a/view?usp=drive_link"
 )
 
 # Loop through each folder/file link and download its contents
