ee89c6f837840385d3a45dc9ca67cf47050594f3
HebatullahHesham <114872807+HebatullahHesham@users.noreply.github.com>
2025-01-09 00:21:42 +0200

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-
 # Create the checkpoints directory
-mkdir -p ./checkpoints
+!mkdir -p Pretrained-models
 
 # Change to the checkpoints directory
-cd ./checkpoints
+%cd Pretrained-models
+
+# Ensure gdown is installed and updated
+!pip install gdown --upgrade
 
 # Download the folders using gdown
-gdown --folder "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS"
-gdown --folder "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc"
-gdown --folder "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl"
-gdown --folder "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0"
-gdown --folder "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU"
-gdown --folder "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4"
+!gdown --folder "https://drive.google.com/drive/folders/1S89SlQ4cxYO_TAjcaJxHNAcnDyL3l_wS"
+!gdown --folder "https://drive.google.com/drive/folders/1LLiZwqogofKkWtzqBILAwWEZybnSMgJc"
+!gdown --folder "https://drive.google.com/drive/folders/1CVGXslFlfwo0eqe4w3ALFNY5X23v4Ipl"
+!gdown --folder "https://drive.google.com/drive/folders/17yfzOsFh0QTc7VfbvHXNt6czWNWdwJM0"
+!gdown --folder "https://drive.google.com/drive/folders/1MuWZ_Cd6kGCtjVB6g7dUFzRSqvryILqU"
+!gdown --folder "https://drive.google.com/drive/folders/1AK62M5S9i09gpRSga4gSv5UUAY2nJ6H4"
